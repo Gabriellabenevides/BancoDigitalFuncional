8a351a20af72f0f9b23f45afbb6eec673fd51f2c
gabi <gabriellabenevides22@gmail.com>
2025-05-21 21:34:37 -0300

    Configuração com banco de dados MySql

---
+++ BancoDigitalFuncional/Domain/Entities/Conta.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entities;
+
+public class Conta
+{
+    public int Id { get; set; }
+    public string NumeroConta { get; set; }
+    public decimal Saldo { get; set; }
+}
---
+++ BancoDigitalFuncional/MySql/MySqlContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MySql;
+
+public class MySqlContext : DbContext
+{
+    public MySqlContext(DbContextOptions<MySqlContext> options) : base(options)
+    {
+    }
+    public DbSet<Conta> Contas { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
